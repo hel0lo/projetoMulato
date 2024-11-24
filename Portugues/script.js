6acbb1d48d4d847c9f2ee83d1bf27c8219b9478b
sarahcamilly <130068428+sarahcamilly@users.noreply.github.com>
2024-11-24 18:53:08 -0300

    Rascunho finalizado.
    tem espaçametos e fontes para ajustar.

--- Portugues/script.js
+++ Portugues/script.js
@@ -1,5 +1,14 @@
-// Função para rolar até a seção correspondente
 function scrollToSection(sectionId) {
     const section = document.getElementById(sectionId);
     section.scrollIntoView({ behavior: 'smooth', block: 'start' });
 }
+const imagemColegio = document.getElementById("imagem-colegio");
+const legendaColegio = document.getElementById("legenda-colegio");
+
+imagemColegio.addEventListener("click", function() {
+    if (legendaColegio.style.display === "block") {
+        legendaColegio.style.display = "none";
+    } else {
+        legendaColegio.style.display = "block";
+    }
+});
