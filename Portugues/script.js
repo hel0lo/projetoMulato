54912e66922ec4555b065763f9164ea8107bccb4
sarahcamilly <130068428+sarahcamilly@users.noreply.github.com>
2024-12-04 09:14:33 -0300

    Pequenos ajustes em espaçamentos.
    Arrumando algumas falhas, VERIFICAR LEGENDA.

--- Portugues/script.js
+++ Portugues/script.js
@@ -3,7 +3,7 @@ function scrollToSection(sectionId) {
     section.scrollIntoView({ behavior: 'smooth', block: 'start' });
 }
 const imagemColegio = document.getElementById("imagem-colegio");
-const legendaColegio = document.getElementById("legenda-colegio");
+const legendaColegio = document.getElementById("legenda");
 
 imagemColegio.addEventListener("click", function() {
     if (legendaColegio.style.display === "block") {
